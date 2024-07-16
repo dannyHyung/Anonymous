7f9e630b18eadb5f0218382c44bfd1aee413510f
dannyHyung <dannykim0516@gmail.com>
2024-07-16 00:37:00 -0400

    project set up

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Anonymous Community</h1>
+      {/* Your components will go here */}
+    </div>
+  );
+}
+
+export default App;
